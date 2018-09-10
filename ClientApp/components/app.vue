6d25a537ee17a2ee7aa98f99e186c826b6d0ac32
German Orekhov <37904421+SlimRhino@users.noreply.github.com>
2018-09-10 12:41:46 +0400

    cleaning

---
+++ ClientApp/components/app.vue
@@ -0,0 +1,19 @@
+﻿<template>
+    <div>
+        <h1>Кажется все заработало</h1>
+        <p>
+            The time is: {{ time }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                time: new Date().toString()
+            };
+        }
+    };
+</script>
